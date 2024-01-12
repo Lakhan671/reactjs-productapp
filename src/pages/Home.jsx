8c42c732e75fc1903690014f1faa59b3b6ce1911
Lakhan Singh <lakhans671@gmail.com>
2024-01-12 09:59:36 +0400

    Update Home.jsx
    filter added

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,18 +1,34 @@
+import React, { useState } from 'react';
 import { useSelector } from 'react-redux';
 import ProductDetails from '../components/products/ProductDetails.jsx';
+
 function HomePage() {
   const products = useSelector((state) => state.products.products);
-  
+  const [filterText, setFilterText] = useState('');
+
+  const filteredProducts = products.filter((product) =>
+    product.category.toLowerCase().includes(filterText.toLowerCase())
+  );
+
   return (
     <div>
-        <section  className="mt-1" id="product-d">
-          <h2>Product List</h2>
-          <ul>
-            {products.map((product) => (
-              <ProductDetails key={product.pid} product={product} />
-            ))}
-          </ul>
-        </section>
+      <section className="mt-1" id="product-d">
+        <h2>Product List</h2>
+        <div className="filter-container">
+          <input
+            type="text"
+            className="form-control filter-input"
+            placeholder="Enter a category"
+            value={filterText}
+            onChange={(e) => setFilterText(e.target.value)}
+          />
+        </div>
+        <ul>
+          {filteredProducts.map((product) => (
+            <ProductDetails key={product.pid} product={product} />
+          ))}
+        </ul>
+      </section>
     </div>
   );
 }
